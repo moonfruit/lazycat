1429352e85fa1b9263b5e6f7121d70a1f76d4040
MoonFruit <dkmoonfruit@gmail.com>
2025-07-01 13:25:34 +0800

    bug fix

--- caddy/dist/init.sh
+++ caddy/dist/init.sh
@@ -1,9 +1,10 @@
 #!/bin/busybox sh
-CONFIG=Caddyfile
-CHECKSUM=$CONFIG.md5
-cd /etc/caddy || exit 1
-if [[ ! -f $CONFIG ]] || md5sum -c $CHECKSUM; then
+DIR=/etc/caddy
+FILE=Caddyfile
+
+cd $DIR || exit 1
+if [[ ! -f $FILE.md5 ]] || md5sum -c $FILE.md5; then
     BASE=$(dirname "$0")
-    cp "$BASE/Caddyfile" $CONFIG
-    md5sum $CONFIG >$CHECKSUM
+    cp "$BASE/$FILE" $DIR
+    md5sum $FILE >$FILE.md5
 fi
