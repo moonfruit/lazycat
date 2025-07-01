bebaed9460d573dbfd0ba29622fe0ab00116d77b
MoonFruit <dkmoonfruit@gmail.com>
2025-07-01 17:40:05 +0800

    Move arai2 Dockerfile to docker

--- aria2/update.sh
+++ aria2/update.sh
@@ -3,23 +3,6 @@ if [[ -z "$PROXY_ENABLED" ]] && hash proxy 2>/dev/null; then
     exec proxy "$0" "$@"
 fi
 
-echo " --- === Updating alpine === ---"
-VERSION=$(skopeo list-tags docker://alpine | jq -r '.Tags
-    | map(select(test("^[0-9]+(\\.[0-9]+){1,2}$")))
-    | sort_by(split(".") | map(tonumber))
-    | last')
-sed -e 's|\(alpine:\).*|\1'"$VERSION"'|' -i docker/Dockerfile
-echo "Using version: $VERSION"
-echo
-
-echo " --- === Updating aria2 === ---"
-VERSION=$(curl -fsSL "https://dl-cdn.alpinelinux.org/alpine/v${VERSION%.*}/community/x86_64/APKINDEX.tar.gz" 2>/dev/null |
-    tar -xzO |
-    awk '$1 == "P:aria2" { found = 1 }; found && $1 ~ /^V:/ { print substr($1, 3); exit }')
-sed -e 's|\(ARIA2_VERSION=\).*|\1'"$VERSION"'|' -i docker/Dockerfile
-echo "Using version: $VERSION"
-echo
-
 source "$ENV/lib/bash/github.sh"
 
 echo " --- === Updating ariang === ---"
@@ -35,8 +18,8 @@ echo "Using version: $VERSION"
 echo
 
 if [[ $1 != "-N" ]]; then
-    if ! git diff --quiet lzc-manifest.yml docker/Dockerfile; then
+    if ! git diff --quiet lzc-manifest.yml; then
         echo " --- === Result === ---"
-        git diff lzc-manifest.yml docker/Dockerfile
+        git diff lzc-manifest.yml
     fi
 fi
