c4250fad901849b695bd1239537f0de85a0d23b7
MoonFruit <dkmoonfruit@gmail.com>
2025-07-03 09:34:17 +0800

    Add crond to update bt-external-ip and bt-tracker

---
+++ aria2/dist/crond/update.sh
@@ -0,0 +1,25 @@
+#!/bin/busybox sh
+fetch() {
+    local key="$1"
+    local url="$2"
+    local value
+    if value=$(curl -fsSL "$url") && [[ -n "$value" ]]; then
+        if [[ -n "$SETTINGS" ]]; then
+            SETTINGS="$SETTINGS,"
+        fi
+        SETTINGS="$SETTINGS\"$key\":\"$value\""
+        echo "$key=$value"
+    fi
+}
+
+fetch bt-external-ip ipv4.ip.sb
+fetch bt-tracker https://fastly.jsdelivr.net/gh/XIU2/TrackersListCollection/best_aria2.txt
+
+if [[ -n "$SETTINGS" ]]; then
+    ID=$(date +%s)
+    JSON='{"id":"'$ID'","jsonrpc":"2.0","method":"aria2.changeGlobalOption","params":["token:aria2",{'"$SETTINGS}]}"
+
+    curl -fsSL -X POST -H 'Content-Type: application/json' \
+        --data "$JSON" http://aria2:6880/jsonrpc
+    echo
+fi
