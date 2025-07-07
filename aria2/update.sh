6485bd72de9516742c6b4393c8226e1a509de87a
MoonFruit <dkmoonfruit@gmail.com>
2025-07-07 17:11:05 +0800

    Remove caddy from aria2

--- aria2/update.sh
+++ aria2/update.sh
@@ -32,17 +32,3 @@ if [[ $OLD != "$NEW" ]]; then
     mv "$TEMP" dist/ariang/index.html
 fi
 echo
-
-# FIXME: Remove this when 1.3.8
-echo " --- === Updating Caddy === ---"
-VERSION=$(find-latest-version caddyserver caddy)
-sed -e 's|\(image: caddy:\).*|\1'"$VERSION"'|' -i lzc-manifest.yml
-echo "Using version: $VERSION"
-echo
-
-if [[ $1 != "-N" ]]; then
-    if ! git diff --quiet lzc-manifest.yml; then
-        echo " --- === Result === ---"
-        git diff lzc-manifest.yml
-    fi
-fi
