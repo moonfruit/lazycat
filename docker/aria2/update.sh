c042cbcbd324861b258e7cea52245f0f2efb78df
MoonFruit <dkmoonfruit@gmail.com>
2025-07-09 14:08:33 +0800

    Refactor to reuse codes

--- docker/aria2/update.sh
+++ docker/aria2/update.sh
@@ -11,21 +11,8 @@ sed -e 's|\(alpine:\).*|\1'"$ALPINE"'|' -i Dockerfile
 echo "Using version: $ALPINE"
 echo
 
-source "$ENV/lib/bash/fs.sh"
-create-temp-file TEMP
-
-find-package-version() {
-    # shellcheck disable=SC2153
-    if [[ ! -s "$TEMP" ]]; then
-        curl -fSL -o "$TEMP" \
-            "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE%.*}/community/x86_64/APKINDEX.tar.gz"
-    fi
-    tar -xzOf "$TEMP" |
-        awk '$1 == "P:'"$1"'" { found = 1 }; found && $1 ~ /^V:/ { print substr($1, 3); exit }'
-}
-
 echo " --- === Updating aria2 === ---"
-VERSION=$(find-package-version aria2)
+VERSION=$(find-alpine-package-version aria2 "${ALPINE%.*}" community)
 sed -e 's|\(ARIA2_VERSION=\).*|\1'"$VERSION"'|' -i Dockerfile
 echo "Using version: $VERSION"
 echo
--- docker/crond/update.sh
+++ docker/crond/update.sh
@@ -11,27 +11,14 @@ sed -e 's|\(alpine:\).*|\1'"$ALPINE"'|' -i Dockerfile
 echo "Using version: $ALPINE"
 echo
 
-source "$ENV/lib/bash/fs.sh"
-create-temp-file TEMP
-
-find-package-version() {
-    # shellcheck disable=SC2153
-    if [[ ! -s "$TEMP" ]]; then
-        curl -fSL -o "$TEMP" \
-            "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE%.*}/main/x86_64/APKINDEX.tar.gz"
-    fi
-    tar -xzOf "$TEMP" |
-        awk '$1 == "P:'"$1"'" { found = 1 }; found && $1 ~ /^V:/ { print substr($1, 3); exit }'
-}
-
 echo " --- === Updating busybox === ---"
-VERSION=$(find-package-version busybox-suid)
+VERSION=$(find-alpine-package-version busybox-suid "${ALPINE%.*}")
 sed -e 's|\(BUSYBOX_VERSION=\)[^ ]*|\1'"$VERSION"'|' -i Dockerfile
 echo "Using version: $VERSION"
 echo
 
 echo " --- === Updating curl === ---"
-VERSION=$(find-package-version curl)
+VERSION=$(find-alpine-package-version curl "${ALPINE%.*}")
 sed -e 's|\(CURL_VERSION=\)[^ ]*|\1'"$VERSION"'|' -i Dockerfile
 echo "Using version: $VERSION"
 echo
