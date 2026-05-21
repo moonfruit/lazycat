a44d326dfd35dee9d5a239e1a6adfaf83b6d27f7
MoonFruit <dkmoonfruit@gmail.com>
2026-05-21 09:39:29 +0800

    Add PvZ2 Gardendless app

---
+++ pvz2/update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+if [[ -z "$PROXY_ENABLED" ]] && hash proxy 2>/dev/null; then
+    exec proxy "$0" "$@"
+fi
+
+source "$ENV/lib/bash/docker.sh"
+
+echo " --- === Updating PvZ2 Gardendless === ---"
+VERSION=$(find-image-latest-version gaozih/pvzge)
+sed -e 's|\(image: gaozih/pvzge:\).*|\1'"$VERSION"'|' \
+    -i lzc-manifest.yml
+echo "Using version: $VERSION"
+echo
+
+if [[ $1 != "-N" ]]; then
+    if ! git diff --quiet lzc-manifest.yml; then
+        echo " --- === Result === ---"
+        git diff lzc-manifest.yml
+    fi
+fi
